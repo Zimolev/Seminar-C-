b66eee044f3aa9b5678d6b5a2ff68a843eed10b4
Zimolev <zimolev@gmail.com>
2022-12-21 11:50:53 +0400

    Added to homework task13 $ task15

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -2,9 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        Console.Write("Введите трехзначное число, если введете не трехзначное ответ будет -1 ");
+        Console.WriteLine("Для задания 10 введите трехзначное число, если введете не трехзначное ответ будет -1 ");
+        Console.WriteLine("Для задания 13 введите любое число");
+        Console.WriteLine("Для задания 15 введите цифру, обозначающую день недели ");
         int number = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine($"Наша вторая цифра числа {number} это {Task10(number)}");
+        //Console.WriteLine($"Наша вторая цифра числа {number} это {Task10(number)}");
+        //Task13(number);
+        Task15(number);
     }
 
     static int Task10(int number)
@@ -22,5 +26,38 @@
             }
             if (count != 3) return -1;
             return number / 10 % 10;
+    }
+
+    static void Task13(int number)
+    /*
+    Задача 13: Напишите программу, 
+    которая выводит третью цифру заданного числа или сообщает,
+    что третьей цифры нет.*/
+    {
+        {
+        int bitNumber = number;
+        int count = 0;
+        
+        if (number < 0) number = 0 - number; 
+            while (bitNumber != 0)
+            {
+                bitNumber = bitNumber / 10;
+                count++;
+            }
+            if (count < 3) Console.WriteLine("Третьей цифры нет");
+            else
+            Console.WriteLine(number % Convert.ToInt32(Math.Pow(10, count - 2)) / Convert.ToInt32(Math.Pow(10, count - 3)));
+    }
+    }
+
+    static void Task15(int number)
+    /*
+    Задача 15: Напишите программу, которая принимает на вход цифру, 
+    обозначающую день недели, и проверяет, является ли этот день выходным.*/
+    {
+        if (number == 7 || number == 6) Console.WriteLine ("Это выходной день");
+        
+        else
+            Console.WriteLine ("Это не выходной день");
     }
 }
