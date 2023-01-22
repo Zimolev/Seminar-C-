a90a35652453366bc2268c290bd3005f9bc4fe9c
zimolev <zimolev@yandex.ru>
2023-01-22 19:36:30 +0400

    Added homework 6 & seminar 6

---
+++ Homework_6/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Task43();
+        System.Console.WriteLine("Введите какое количество чисел вы хотите ввести: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        int count = Task41(m);
+        System.Console.WriteLine($"Вы ввели {count} положительных числа");
+    }
+
+    /*Задача 43: Напишите программу, которая найдёт точку 
+    пересечения двух прямых, 
+    заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+    значения b1, k1, b2 и k2 задаются пользователем.
+y = k1 * x + b1, y = k2 * x + b2
+k1 * x + b1 = k2 * x + b2
+(k1 - k2) * x = b2 - b1
+x = (b2 - b1) / (k1 - k2)
+y = k1 * x + b1
+*/
+    static void Task43()
+    {
+        System.Console.WriteLine("Введите значение b1: ");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+        System.Console.WriteLine("Введите значение k1: ");
+        double k1 = Convert.ToDouble(Console.ReadLine());
+        System.Console.WriteLine("Введите значение b2: ");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+        System.Console.WriteLine("Введите значение k2: ");
+        double k2 = Convert.ToDouble(Console.ReadLine());
+        if (k1 == k2 && b1 == b2) System.Console.WriteLine("Прямые совпадают");
+        if (k1 ==k2) System.Console.WriteLine("Прямые параллельны");
+        double x = (b2-b1)/(k1-k2);
+        double y = (k1*(b2-b1))/(k1-k2)+b1;
+        System.Console.WriteLine($"Точка пересечения прямых x = {x}, y = {y}");
+    }
+
+    /*Задача 41: Пользователь вводит с клавиатуры M чисел.
+     Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+
+     static int Task41(int m)
+     {
+        int count = 0;
+        for (int i = 1; i <= m; i++)
+        {
+            System.Console.WriteLine($"Введите {i}-ое число");
+            double number = Convert.ToDouble(Console.ReadLine());
+            if (number > 0) count++;
+        }
+
+        return count;
+     }
+}
---
+++ Seminar6/Program.cs
@@ -0,0 +1,90 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter the size of the array: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Enter the minimum value of the array ");
+        int minValue = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Enter the maximum value of the array ");
+        int maxValue = Convert.ToInt32(Console.ReadLine());
+        
+        //int[] array = Fibonachi(size, minValue, maxValue);
+        int[] array = RandomArrey(size, minValue, maxValue);
+        ShowArray(array);
+        //ReserveArray(array);
+        //ShowArray(array);
+        //System.Console.WriteLine(Treugulnik(size, minValue, maxValue));
+        int[] copyArray = CopyAray(array);
+        array[0] = -1000;
+        ShowArray(array);
+        ShowArray(copyArray);
+
+    }
+
+    static void ReserveArray(int[] array)
+    {
+        for (int i = 0, j = array.Length - 1;  i <j; i++, j--)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    static int[] RandomArrey(int size, int minValue, int maxValue)
+    {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = new Random().Next(minValue, maxValue + 1);
+        }
+        return array;
+    }
+
+    static void ShowArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+/*Напишите программу, которая принимает на вход три числа
+ и проверяет, может ли существовать треугольник
+ с сторонами такой длины.*/
+    static bool Treugulnik(int a, int  b, int c)
+    {
+        return (a + b > c && a + c > b && c + b > a);
+    }
+
+/* Не используя рекурсию, 
+выведите первые N чисел Фибоначчи. 
+Первые два числа Фибоначчи: a и b.*/
+    static int[] Fibonachi(int n, int a, int b)
+    {
+        int[] array = new int[n];
+        array[0] = a;
+        array[1] = b;
+        for (int i = 2; i < array.Length; i++)
+        {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+        return array;
+    }
+
+/* Напишите программу, которая будет создавать
+ копию заданного массива с помощью 
+ поэлементного копирования.*/
+
+ static int[] CopyAray(int[] array)
+ {
+    int[] copyArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        copyArray[i] = array[i];
+    }
+    return copyArray;
+ }
+}
